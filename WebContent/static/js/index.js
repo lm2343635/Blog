6cb28508d1b920766735673f61ea58736ee65578
limeng <lm2343635@126.com>
2016-05-20 23:17:17 +0900

    完成前端国际化

--- WebContent/static/js/index.js
+++ WebContent/static/js/index.js
@@ -1,4 +1,21 @@
 $(document).ready(function($) {
 
 	$("#works").load("work.html");
+	
+	i18n("index", "static/i18n/", [
+		 "index_blogs",
+		 "index_introduce",
+		 "index_contact_me",
+		 "index_my_blogs",
+		 "index_my_github",
+		 "index_about",
+		 "index_about_content_1",
+		 "index_about_content_2",
+		 "index_about_content_3",
+		 "index_about_content_4",
+		 "index_about_content_5",
+		 "index_motto",
+		 "index_contact",
+		 "index_address"
+   	]);
 });
