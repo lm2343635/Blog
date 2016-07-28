769de1fb5bd14dd8526d53c94f039a1f703e6e58
limeng <lm2343635@126.com>
2016-07-29 00:11:39 +0900

    实现读写JSON配置文件管理员账户接口

--- src/org/fczm/blog/service/impl/AdminManagerImpl.java
+++ src/org/fczm/blog/service/impl/AdminManagerImpl.java
@@ -13,18 +13,31 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class AdminManagerImpl extends ManagerTemplate implements AdminManager {
+	private static final String CONFIG_PATH = "WEB-INF/config.json";
+	private JsonTool config = null;
 	private	JSONArray admins = null;
-
+	
+	public JsonTool getConfig() {
+		if(config == null) {
+			String pathname = WebContextFactory.get().getServletContext().getRealPath("/") + File.separator + CONFIG_PATH;
+			config = new JsonTool(pathname);
+		}
+		return config;
+	}
+	
+	public JSONArray getAdmins() {
+		if(admins == null) {
+			admins = getConfig().getJSONArray("admins");
+		}
+		return admins;
+	}
+	
 	@Override
 	public boolean login(String username, String password, HttpSession session) {
-		if(admins == null) {
-			String pathname = WebContextFactory.get().getServletContext().getRealPath("/") + File.separator + "WEB-INF/config.json";
-			JsonTool config = new JsonTool(pathname);
-			admins = config.getJSONArray("admins");
-		}
-		for(int i=0; i<admins.size(); i++) {
-			JSONObject admin = (JSONObject) admins.get(i);
-			if(username.equals(admin.getString("username"))&&password.equals(admin.getString("password"))) {
+		getAdmins();
+		for(int i = 0; i < admins.size(); i++) {
+			JSONObject admin = admins.getJSONObject(i);
+			if(username.equals(admin.getString("username")) && password.equals(admin.getString("password"))) {
 				session.setAttribute(ADMIN_FLAG, username);
 				return true;
 			}	
@@ -38,6 +51,65 @@ public class AdminManagerImpl extends ManagerTemplate implements AdminManager {
 			return null;
 		}
 		return (String)session.getAttribute(ADMIN_FLAG);
+	}
+
+	@Override
+	public boolean addAdmin(String username, String password) {
+		if(username.equals("") || password.equals("")) {
+			return false;
+		}
+		getAdmins();
+		for(int i = 0; i < admins.size(); i++) {
+			if(username.equals(admins.getJSONObject(i).getString("username"))) {
+				return false;
+			}	
+		}
+		JSONObject admin = new JSONObject();
+		admin.put("username", username);
+		admin.put("password", password);
+		admins.add(admin);
+		config.put("admins", admins);
+		config.writeObject();
+		return true;
+	}
+
+	@Override
+	public boolean modifyPassword(String username, String oldPassword, String newPassword) {
+		getAdmins();
+		for(int i = 0; i < admins.size(); i++) {
+			JSONObject admin = admins.getJSONObject(i);
+			if(username.equals(admin.getString("username")) && oldPassword.equals(admin.getString("password"))) {
+				admin.put("password", newPassword);
+				admins.set(i, admin);
+				config.put("admins", admins);
+				config.writeObject();
+				return true;
+			}	
+		}
+		return false;
+	}
+
+	@Override
+	public boolean removeAdmin(String username, String password) {
+		getAdmins();
+		for(int i = 0; i < admins.size(); i++) {
+			JSONObject admin = admins.getJSONObject(i);
+			if(username.equals(admin.getString("username")) && password.equals(admin.getString("password"))) {
+				admins.remove(i);
+				config.put("admins", admins);
+				config.writeObject();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int getAdminPageSize() {
+		return config.getInt("adminPageSize");
+	}
+
+	public int getUserPageSize() {
+		return config.getInt("userPageSize");
 	}
 
 }
